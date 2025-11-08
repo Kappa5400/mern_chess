15eac5b73100771b9f5f7ef20ad3dc8bf7c1b754
Kappa5400 <kappa6708@gmail.com>
2025-11-08 18:25:31 +0000

    chore: fixed routes

--- chess/backend/src/routes/r_puzzle.js
+++ chess/backend/src/routes/r_puzzle.js
@@ -1,8 +1,11 @@
+import { puzzle } from "../db/puzzle.js";
 import {
   retrieveDailyPuzzle,
   getPuzzleByID,
   get_all_puzzles,
 } from "../service/s_puzzle.js";
+
+import mongoose from "mongoose";
 
 export function routes(app) {
   app.post("/api/v1/get_dailypuzzle", async (req, res) => {
@@ -12,12 +15,13 @@ export function routes(app) {
 
   app.get("/api/v1/get_all_puzzles", async (req, res) => {
     const all_puzzles = await get_all_puzzles();
-    return all_puzzles.jsonify();
+    return res.json(all_puzzles);
   });
 
-  app.get("api/v1/puzzle/:id", async (req, res) => {
+  app.get("/api/v1/puzzle/:id", async (req, res) => {
     const { id } = req.params;
-    const puzzle = await getPuzzleByID(id);
-    return puzzle.jsonify();
+    const hit_puzzle = await puzzle.findById(id);
+    if (!hit_puzzle) return res.status(404).json({ error: "Puzzle not found" });
+    return res.json(hit_puzzle);
   });
 }
