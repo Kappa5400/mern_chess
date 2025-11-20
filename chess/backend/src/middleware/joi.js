5c27366e7025af300dc84d25b403ba51f21156d7
colin <colinburdett54@gmail.com>
2025-11-19 21:10:44 -0600

    chore: updating joi, todolist, need to impliment joi in routing, do user joi schemas

--- chess/backend/src/middleware/joi.js
+++ chess/backend/src/middleware/joi.js
@@ -1,5 +1,25 @@
-import joi from "joi";
+import Joi from "joi";
 
 export const createPuzzleSchema = Joi.object({
-  pgn: joi.string().min(5).required(),
+  pgn: Joi.string().min(5).required(),
+  answer: Joi.string().required(),
+  rating: Joi.number().integer().min(0).max(2900).required(),
+  date: Joi.date(),
 });
+
+export const updatePuzzleSchema = Joi.object({
+  pgn: Joi.string().min(5),
+  answer: Joi.string(),
+  rating: Joi.number().integer().min(0).max(2900),
+});
+
+export const validate = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ Error: error.details[0].message });
+    }
+    next();
+  };
+};
