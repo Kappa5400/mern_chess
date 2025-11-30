46218e675d74320e7132eddf9c29d1237498b41d
colin <colinburdett54@gmail.com>
2025-11-30 10:39:26 -0600

    chore: ugh

--- chess/backend/src/app.js
+++ chess/backend/src/app.js
@@ -1,18 +1,18 @@
 import express from "express";
-//import cors from "cors";
-//import morgan from "morgan";
-//import helmet from "helmet";
-//import { puzzleRoutes } from "./routes/r_puzzle.js";
-//import { userPuzzleRoutes } from "./routes/r_userPuzzle.js";
-//import { userRoutes } from "./routes/r_user.js";
-//import swaggerUi from "swagger-ui-express";
-//import { swaggerSpec } from "./swagger/swaggerOptions.js";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import { puzzleRoutes } from "./routes/r_puzzle.js";
+import { userPuzzleRoutes } from "./routes/r_userPuzzle.js";
+import { userRoutes } from "./routes/r_user.js";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./swagger/swaggerOptions.js";
 
 const app = express();
-//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json());
-/*
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
@@ -25,15 +25,15 @@ if (process.env.NODE_ENV === "development") {
 } else {
   app.use(morgan("combined"));
 }
-*/
+
 app.get("/", (req, res) => {
   console.log("--- DEBUG: Reached GET / route ---");
   res.send("Hi sekai");
 });
 
-//userRoutes(app);
-//userPuzzleRoutes(app);
-//puzzleRoutes(app);
+userRoutes(app);
+userPuzzleRoutes(app);
+puzzleRoutes(app);
 
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
