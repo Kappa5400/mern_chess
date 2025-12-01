0a593b429f7225b63f8b886dd23650b42a756c8c
colin <colinburdett54@gmail.com>
2025-11-30 19:22:48 -0600

    chore: middleware helmet sanatize compress implimentated

--- chess/backend/src/app.js
+++ chess/backend/src/app.js
@@ -2,21 +2,47 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
+import compression from "compression";
 import { puzzleRoutes } from "./routes/r_puzzle.js";
 import { userPuzzleRoutes } from "./routes/r_userPuzzle.js";
 import { userRoutes } from "./routes/r_user.js";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./swagger/swaggerOptions.js";
+import mongoSanitize from "express-mongo-sanitize";
 
 const app = express();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json());
+app.use(helmet());
+app.use(compression());
 
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+  })
+);
+
+// in order to sanatize
+// allows newer express to edit query req after recieving
+app.use((req, res, next) => {
+  Object.defineProperty(req, "query", {
+    ...Object.getOwnPropertyDescriptor(req, "query"),
+    value: req.query,
+    writable: true,
+  });
+  next();
+});
+
+//sanatize below
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+    // only sanitize body & params, not query
+    onSanitize: ({ req, key }) => {
+      if (key === "query") return false;
+    },
   })
 );
 
