0a440f1707b504f54e12169373038e6695150d4b
colin <colinburdett54@gmail.com>
2025-12-28 11:53:40 -0600

    chore: cicd wip

--- chess/backend/src/__tests__/integration/userRoutes.test.js
+++ chess/backend/src/__tests__/integration/userRoutes.test.js
@@ -2,6 +2,7 @@ import request from "supertest";
 import mongoose, { mongo } from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import app from "../../app.js";
+import { createUser } from "../../service/s_user.js";
 
 let mongoServer;
 
@@ -15,10 +16,26 @@ beforeAll(async () => {
   await mongoose.connect(uri);
 });
 
+beforeEach(async () => {
+  for (const col of Object.values(mongoose.connection.collections)) {
+    await col.deleteMany({});
+  }
+});
+
 afterAll(async () => {
   await mongoose.disconnect();
   await mongoServer.stop();
 });
+
+const createDummyUser = (overrides = {}) => ({
+  username: "name",
+  password: "password",
+  score: 0,
+  user_puzzles: 0,
+  ...overrides,
+});
+
+const saveDummy = async (overrides) => User.create(createDummyUser(overrides));
 
 describe("User routes", () => {
   it("POST /api/v1/user/signup - create user", async () => {
@@ -32,9 +49,11 @@ describe("User routes", () => {
   });
 
   it("POST /api/v1/user/login - login", async () => {
+    await createUser({ username: "test", password: "pass" });
+
     const res = await request(app).post("/api/v1/user/login").send({
-      username: "test1",
-      password: "password1",
+      username: "test",
+      password: "pass",
     });
 
     expect(res.statusCode).toBe(200);
