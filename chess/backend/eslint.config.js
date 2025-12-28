aefb311c3e0ee3802c579c409700527d5499dfb1
colin <colinburdett54@gmail.com>
2025-12-28 11:22:30 -0600

    chore: adding eslint config to backend

---
+++ chess/backend/eslint.config.js
@@ -0,0 +1,44 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist", "node_modules", "build"]),
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser, // Frontend用
+        ...globals.node, // Backend (processなど) 用
+        ...globals.jest, // Test (describe, itなど) 用
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^[A-Z_]",
+          argsIgnorePattern: "^_", // 引数で使わないものは _req のように書けば許容する設定
+        },
+      ],
+      "no-undef": "error",
+    },
+  },
+]);
