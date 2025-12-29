f0e2e14d7ed2b65a68e8e369c0bc96f9a039f27e
colin <colinburdett54@gmail.com>
2025-12-29 05:49:41 -0600

    chore: fixes to hardcoded env var

--- chess/src/api/api_user.js
+++ chess/src/api/api_user.js
@@ -1,5 +1,4 @@
-//update befor deployment
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const getHeadersToken = () => {
   const token = localStorage.getItem("token");
--- chess/src/api/api_user_puzzle.js
+++ chess/src/api/api_user_puzzle.js
@@ -1,5 +1,4 @@
-//update before deployment
-const BASE_URL = "http://localhost:3001/";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const getHeaders = () => {
   const token = localStorage.getItem("token");
