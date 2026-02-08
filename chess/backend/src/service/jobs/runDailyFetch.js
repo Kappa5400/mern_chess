b66dc6b8e510d09869fc025671c85ad89e0958f4
colin <colinburdett54@gmail.com>
2026-02-08 08:49:23 -0600

    chore: on each successful puzzle script run delete oldest in db

--- chess/backend/src/service/jobs/runDailyFetch.js
+++ chess/backend/src/service/jobs/runDailyFetch.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 import { getDailyPuzzle } from "../s_puzzle.js";
 import "dotenv/config";
 import { logger } from "../../utils/logger.js";
+import { deleteOldest } from "../s_puzzle.js";
 
 const run = async () => {
   logger.info("Starting dailypuzzle fetch script");
+  let del_flag = 0;
   logger.info("Connecting to DB...");
   try {
     logger.info("Loaded DB_URL:", process.env.DB_URL);
@@ -17,10 +19,23 @@ const run = async () => {
   logger.info("Attempting to get daily puzzle from manual script");
   try {
     await getDailyPuzzle();
+    del_flag = 1;
   } catch (err) {
     logger.info("Error getting daily puzzle: ", err);
     process.exit(2);
   }
+
+  if (del_flag == 1) {
+    try {
+      logger.info("Attempting to delete oldest in db...");
+      deleteOldest();
+      logger.info("Oldest deleted.");
+    } catch (err) {
+      logger.info("Error deleting oldest puzzle in db: ", err);
+    }
+  }
+  del_flag = 0;
+
   await mongoose.connection.close();
   logger.info("Closed db connection.");
   process.exit(0);
